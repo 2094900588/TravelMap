194885673afd0f2e043540368356e54327fa877b
lyh <1550777295@qq.com>
2023-05-08 20:46:45 +0800

    项目创建好了

---
+++ backend/src/main/java/com/example/zuinbxm/ZuinbxmApplication.java
@@ -0,0 +1,13 @@
+package com.example.zuinbxm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuinbxmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuinbxmApplication.class, args);
+    }
+
+}
